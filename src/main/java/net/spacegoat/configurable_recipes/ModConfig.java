67bd697ceb3c173fdcb7971dfe49aec089a9bf0f
spacegoat3 <70437195+spacegoat3@users.noreply.github.com>
2021-12-22 18:59:02 -0800

    Added files

--- src/main/java/net/spacegoat/configurable_recipes/ModConfig.java
+++ src/main/java/net/spacegoat/configurable_recipes/ModConfig.java
@@ -5,6 +5,7 @@ import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 @Config(name = ModMain.MOD_ID)
 @Config.Gui.Background("minecraft:textures/block/crafting_table_top.png")
@@ -29,16 +30,22 @@ public class ModConfig implements ConfigData {
     }
     public static class Recipes {
         @ConfigEntry.Gui.RequiresRestart
+        @Comment("6 Source Material + 1 Leather = Horse Armor")
         public boolean enableHorseArmorRecipes = true;
         @ConfigEntry.Gui.RequiresRestart
+        @Comment("5 Leather + 1 Iron Ingot + 2 Tripwire Hook = Saddle")
         public boolean enableSaddleRecipe = true;
         @ConfigEntry.Gui.RequiresRestart
+        @Comment("4 Yellow Wool + 4 Any Type of Seed + 1 Magma Cream = Sponge")
         public boolean enableSpongeRecipe = true;
         @ConfigEntry.Gui.RequiresRestart
+        @Comment("4 Redstone + 5 Gold Nuggets = Glowstone Dust")
         public boolean enableGlowstoneDustRecipe = true;
         @ConfigEntry.Gui.RequiresRestart
+        @Comment("2 Maps + 3 Strings = Name Tag")
         public boolean enableNameTagRecipe = true;
         @ConfigEntry.Gui.RequiresRestart
+        @Comment("Makes all saplings craftable into stick.")
         public boolean saplingToStick = true;
     }
     public static class RottenFleshToLeather{
